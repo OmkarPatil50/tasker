54134291377c9e5dc1f489c10716b0c8d393ada1
Omkar Patil <Omkarp5429@gmail.com>
2023-11-28 16:09:26 +0530

    Create: Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import TaskList from './pages/TaskList/TaskList';
+import TaskForm from './pages/TaskForm/TaskForm';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<TaskList />} />
+        <Route path='/add' element={<TaskForm />} />
+        <Route path='/edit/:taskId' element={<TaskForm />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import { TaskContext } from "../../context/TaskContextProvider";
+
+export function Navbar() {
+  const { dispatch } = useContext(TaskContext);
+  const navigate = useNavigate();
+  return (
+    <nav className="navbar">
+      <h1 className="app-heading">Tasker</h1>
+      <input
+        className="search-bar-nav"
+        type="search"
+        placeholder="Search movies by title, cast and director..."
+        onChange={(event) => {
+          navigate("/");
+          dispatch({ type: "UPDATE_SEARCH_TEXT", payload: event.target.value });
+        }}
+      />
+    </nav>
+  );
+}
